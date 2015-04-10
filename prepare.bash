cae571ab474715208eaaf52f31e3c5bca9df2df9
Casey Marshall <cmars@cmarstech.com>
2015-04-09 23:22:28 -0500

    Release is 2.0~b1

--- prepare.bash
+++ prepare.bash
@@ -3,7 +3,7 @@
 export DEBEMAIL="cmars@cmarstech.com"
 export DEBFULLNAME="Casey Marshall"
 
-export RELEASE_VERSION=2.0~a2
+export RELEASE_VERSION=2.0~b1
 
 export BUILD_PACKAGE=github.com/hockeypuck/server
 
