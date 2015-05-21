9bbda70246248be61e7ec0b6470a1844c84be020
Casey Marshall <cmars@cmarstech.com>
2015-05-20 23:34:33 -0500

    Fix cross-compile script -- use local gox, create dist dir.

--- build-xc.bash
+++ build-xc.bash
@@ -3,10 +3,12 @@
 . prepare.bash
 
 for cmdpkg in $(go list ${BUILD_PACKAGE}/cmd/...); do
-	gox -os '!windows !netbsd !plan9' \
+	$GOPATH/bin/gox -os '!windows !netbsd !plan9' \
 		-output 'build/{{.OS}}_{{.Arch}}/usr/bin/{{.Dir}}' \
 		$cmdpkg
 done
+
+mkdir -p dist
 
 for osarch in $(ls build); do
 	tarfile=dist/hockeypuck-${PACKAGE_VERSION}-${osarch}.tar
